ebd0d8fe8c0d1d0f9fa6da1909709a0d9b641832
toymurrahman <toymurrahman2@gmail.com>
2025-02-03 23:49:46 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ function App() {
   return (
     <>
    
-      <h1 className='text-red-600'>Vite +jjjjjjj React</h1>
+      <h1 className='text-red-600'>Vite +jjjjjjjgit config --local user.email
+      React</h1>
       <button className="btn btn-outline btn-accent">Accent</button>
     
     </>
