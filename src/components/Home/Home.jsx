82936f4a8689fc16792df7d919079c4e3c02fc9f
toymurrahman <rahmantoymur2@gmail.com>
2025-02-04 01:06:23 +0600

    email check

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <h1>This is home: hoooooooo</h1>
         </div>
     );
 };
